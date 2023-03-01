1e479e2c65bfad0d2625c5e023f0705d9eaaf461
juanfatur <jffatur@gmail.com>
2023-03-01 18:59:09 -0300

    actualizado

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("July 1, 2023").getTime();
+    const countDate = new Date("December 10, 2023").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
